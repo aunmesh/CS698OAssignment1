1514c4d1ef6314434e0dc4ac802e9e336b2a479f
Asim Unmesh <a.unmesh@gmail.com>
2017-01-25 22:47:49 +0530

    wrote test script to save all the features of images in a 2 level database

--- test1.py
+++ test1.py
@@ -1,16 +1,20 @@
 import cv2
 import numpy as np
+import sys
+import os
+from glob import glob
 
-img = cv2.imread('N2_330.jpg')
-gray= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+sys.path.append(os.getcwd())
+import globalvariables #script storing global variables such as location of root folder of image database
+subdir = glob(rootfolder + "/*/") #rootfolder is specified in global variables without a trailing /
 
-
-sift = cv2.xfeatures2d.SIFT_create(edgeThreshold = 500)
-(kps, descs) = sift.detectAndCompute(gray, None)
-
-#sift = cv2.SIFT()
-#kp = sift.detect(gray,None)
-
-cv2.drawKeypoints(gray,kps,img)
-
-cv2.imwrite('sift_keypoints_01.jpg',img)
+for temp in subdir:
+	os.chdir(temp)
+	imagefiles = glob(temp)
+	for l in imagefiles:
+		img = cv2.imread(l)
+		gray= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+		sift = cv2.xfeatures2d.SIFT_create(edgeThreshold = 500)
+		(kps, descs) = sift.detectAndCompute(gray, None)
+		imgname = l.split(".")[-2]
+		np.save(imgname,descs)
